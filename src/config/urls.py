b842ed959dd70b9f93fef42096173fcaf800e822
Udit <23015406+udit-001@users.noreply.github.com>
2024-07-12 09:55:00 +0530

    feat: add project files

---
+++ src/config/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+from app.views import *
+
+urlpatterns = [
+    path('', submit_url, name="start-video"),
+    path('video', watch_video, name="watch-video"),
+    path('api/videos/', VideoCreateView.as_view(), name="video-create"),
+    path('api/videos/<int:pk>/notes/', VideoNoteView.as_view(), name="note-list-create"),
+    path('admin/', admin.site.urls),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+]
